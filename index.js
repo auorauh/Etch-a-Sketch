dea2e4244be511751fa5d696163ee5e786742131
auorauh <dr.teasdale.trenton@gmail.com>
2021-12-30 17:19:22 -0700

    bigger canv

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let cnvSize = 16;
+let cnvSize = 36;
 
 // create individual div pixels
 for (i=0; i<cnvSize; i++){
@@ -18,15 +18,18 @@ canvas.appendChild(pixel);
 
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', () => {
-   for (i=0; i<cnvSize; i++){ 
-   let reset = document.querySelector('.line');
-reset.classList.replace('line','pixel')
-   }
+   blank();
 });
 
+function blank() {
+   for (i=0; i<cnvSize; i++){ 
+      let reset = document.querySelector('.line');
+   reset.classList.replace('line','pixel');
+}
+}
 const sml = document.querySelector('#small');
 sml.addEventListener('click', () => {
-   if (cnvSize = 32 || cnvSize == 64) {
+   if (cnvSize = 64 || cnvSize == 128) {
       downSize = document.querySelector('#canvas');
       downSize.classList.remove('gridL');
       downSize.classList.remove('gridM');
@@ -34,27 +37,26 @@ sml.addEventListener('click', () => {
    }
    let gridS = document.querySelector('#canvas');
    gridS.classList.replace('canvas', 'gridS');
-   cnvSize = 16;
+   cnvSize = 36;
    createCnv();
 });
 const med = document.querySelector('#med');
 med.addEventListener('click', () => {
-   if (cnvSize = 16 || cnvSize == 64) {
+   if (cnvSize = 36 || cnvSize == 128) {
       downSize = document.querySelector('#canvas');
       downSize.classList.remove('gridL');
       downSize.classList.remove('gridS');
       downSize.classList.add('canvas');
    }
    let gridM = document.querySelector('#canvas');
-   console.log(gridM)
    gridM.classList.replace('canvas', 'gridM');
-   cnvSize = 32;
+   cnvSize = 64;
    createCnv();
 });
 
 const large = document.querySelector('#large');
 large.addEventListener('click', () => {
-   if (cnvSize = 16 || cnvSize == 32) {
+   if (cnvSize = 36 || cnvSize == 64) {
       downSize = document.querySelector('#canvas');
       downSize.classList.remove('gridM');
       downSize.classList.remove('gridS');
@@ -62,14 +64,12 @@ large.addEventListener('click', () => {
    }
 
    let gridL = document.querySelector('#canvas');
-   console.log(gridL)
    gridL.classList.replace('canvas', 'gridL');
-   cnvSize = 64;
+   cnvSize = 128;
    createCnv();
 });
 
 function createCnv() {
-
    for (i=0; i<cnvSize; i++){
       const makeCanvas = document.querySelector('#canvas');
       
